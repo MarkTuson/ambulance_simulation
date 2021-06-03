7c8c85d76b7dc7dc4e356ee0c9fadb1e1f415fc4
Geraint Palmer <palmer.geraint@googlemail.com>
2021-06-03 17:28:10 +0100

    add main script to run trials of simulations

--- src/code/main.py
+++ src/code/main.py
@@ -1,2 +1,34 @@
-def test_function():
-    pass
+"""
+Usage:
+    main.py <params_name> <results_name> <n_trials>
+Arguments
+    params_name   : name of the parameter yml file
+    results_name  : name of the results file to write
+    n_trials      : number of trials to run
+"""
+import sys
+from methods import *
+import ciw
+import yaml
+import pandas as pd
+
+if __name__ == '__main__':
+    args = sys.argv
+    params_name = args[1]
+    results_name = args[2]
+    n_trials = int(args[3])
+
+    with open(params_name, 'r') as f:
+        params = yaml.load(f, Loader=yaml.CLoader)
+
+    all_recs = []
+    for trial in range(n_trials):
+        ciw.seed(trial)
+        N = create_ambulance_network(params)
+        Q = AmbulanceSimulation(N, node_class=AmbulanceNode, individual_class=Patient, params=params)
+        Q.simulate_until_max_time(100)
+        recs = pd.DataFrame(Q.get_all_records())
+        recs['Trial'] = trial
+        all_recs.append(recs)
+
+    pd.concat(all_recs).to_csv(results_name)
