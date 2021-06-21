217ce69adc871471eae39461422db3a1e34156d9
Mark Tuson <tusonm@cardiff.ac.uk>
2021-06-21 10:08:11 +0100

    Formatting

--- src/code/main.py
+++ src/code/main.py
@@ -12,23 +12,25 @@ import ciw
 import yaml
 import pandas as pd
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = sys.argv
     params_name = args[1]
     results_name = args[2]
     n_trials = int(args[3])
 
-    with open(params_name, 'r') as f:
+    with open(params_name, "r") as f:
         params = yaml.load(f, Loader=yaml.CLoader)
 
     all_recs = []
     for trial in range(n_trials):
         ciw.seed(trial)
         N = create_ambulance_network(params)
-        Q = AmbulanceSimulation(N, node_class=AmbulanceNode, individual_class=Patient, params=params)
+        Q = AmbulanceSimulation(
+            N, node_class=AmbulanceNode, individual_class=Patient, params=params
+        )
         Q.simulate_until_max_time(100)
         recs = pd.DataFrame(Q.get_all_records())
-        recs['Trial'] = trial
+        recs["Trial"] = trial
         all_recs.append(recs)
 
     pd.concat(all_recs).to_csv(results_name)
