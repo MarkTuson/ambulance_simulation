dc0035ee29941f15cf38da54c2bb5b43c3958d08
Geraint Palmer <palmer.geraint@googlemail.com>
2022-07-08 16:06:16 +0100

    add sequential rrv simulation

--- src/code/main.py
+++ src/code/main.py
@@ -11,6 +11,7 @@ from methods import *
 import ciw
 import yaml
 import pandas as pd
+import numpy as np
 
 if __name__ == "__main__":
     args = sys.argv
@@ -24,12 +25,35 @@ if __name__ == "__main__":
     all_recs = []
     for trial in range(n_trials):
         ciw.seed(trial)
-        N = create_transit_network(params)
-        Q = TransitSimulation(
-            N, node_class=TransitNode, individual_class=TransitJob, params=params
+        N_transit = create_transit_network(params)
+        Q_transit = TransitSimulation(
+            N_transit, node_class=TransitNode, individual_class=TransitJob, params=params
         )
-        Q.simulate_until_max_time(31, progress_bar=True)
-        recs = pd.DataFrame(Q.get_all_records())
+        Q_transit.simulate_until_max_time(31, progress_bar=True)
+        recs_transit = pd.DataFrame(Q_transit.get_all_records())
+        recs_transit = recs_transit[recs_transit['destination'] == -1]
+        
+        initial_recs = recs_transit[recs_transit['ambulance_location'] != 'False'].sort_values('call_date').reset_index()
+        N_response = create_response_network(params, initial_recs)
+        Q_response = ResponseSimulation(
+            N_response,
+            arrival_node_class=ResponseArrivalNode,
+            individual_class=ResponseJob,
+            node_class=ResponseNode,
+            params=params,
+            initial_recs=initial_recs
+        )
+        Q_response.simulate_until_max_time(31, progress_bar=True)
+        recs_response = Q_response.get_all_records()
+        recs_response = pd.DataFrame(recs_response)
+        recs_response = recs_response[recs_response['rrv_location'] != -1]
+        
+        recs_response = recs_response.set_index('id_number')
+        recs_transit = recs_transit.set_index('id_number')
+        recs = pd.concat([recs_transit, recs_response], axis=1)
+        recs.replace(to_replace=False, value=np.NAN, inplace=True, method=None)
+        recs['response_time'] = recs[['ambulance_pick_up_time', 'rrv_pick_up_time']].min(axis=1)
+        recs['rrv_action'] = recs.apply(classify, axis=1)
         recs["Trial"] = trial
         all_recs.append(recs)
 
--- src/code/methods.py
+++ src/code/methods.py
@@ -2,6 +2,16 @@ import ciw
 import pandas as pd
 import math
 from collections import namedtuple
+import numpy as np
+
+def classify(row):
+    if np.isnan(row['rrv_pick_up_time']):
+        return 'RRV not deployed'
+    if row['rrv_delay_at_site'] == 0.0:
+        return 'RRV arrived too late'
+    if row['rrv_pick_up_time'] > row['ambulance_pick_up_time']:
+        return 'RRV arrived after ambulance'
+    return 'RRV arrived before ambulance'
 
 def convert_from_class(clss, params):
     k = clss % params["n_specialities"]
@@ -310,5 +320,200 @@ def create_transit_network(params):
             ]
             for c in range(params["n_locations"] * params['n_specialities'])
         },
+    )
+    return N
+
+
+class ResponseSimulation(ciw.Simulation):
+    def __init__(
+        self,
+        network,
+        tracker=ciw.trackers.StateTracker(),
+        node_class=None,
+        arrival_node_class=None,
+        individual_class=None,
+        server_class=None,
+        params=None,
+        initial_recs=None,
+    ):
+        self.params = params
+        self.initial_recs = initial_recs
+        super().__init__(
+            network,
+            exact=False,
+            name="Simulation",
+            tracker=tracker,
+            deadlock_detector=ciw.deadlock.NoDetection(),
+            node_class=node_class,
+            arrival_node_class=arrival_node_class,
+            individual_class=individual_class,
+            server_class=server_class,
+        )
+
+
+class ResponseJob(ciw.Individual):
+    def __init__(self, id_number, row, customer_class=0, priority_class=0, simulation=False):
+        super().__init__(id_number, customer_class, priority_class, simulation)
+        self.ambulance_pick_up_time = row['ambulance_pick_up_time']
+        self.expected_ambulance_pick_up_time = row['ambulance_expected_pick_up_time']
+        self.ambulance_delay_at_site = row['ambulance_delay_at_site']
+        self.speciality = row['speciality']
+        self.pick_up_location = row['pick_up_location']
+        self.pick_up_time = False
+        self.delay_at_site = False
+        self.return_to_loc_time = False
+        self.rrv = False
+
+
+ResponseDataRecord = namedtuple(
+    "ResponseRecord",
+    [
+        "id_number",
+        "rrv_location",
+        "rrv_id",
+        "rrv_service_time",
+        "rrv_pick_up_time",
+        "rrv_delay_at_site",
+        "rrv_return_to_loc_time"
+    ],
+)
+
+
+class ResponseNode(ciw.Node):
+    def next_node(self, ind):
+        """
+        Decides on the next node.
+
+        If this is the first node:
+          - find all the free ambulances
+          - if no free ambulances, leave
+          - calculate expected pick up time for each free ambulance
+          - 'book' the closest ambulance, e.g. choose that node
+        """
+        if self.id_number != 1:
+            return self.simulation.nodes[-1]
+        else:
+            choices = {
+                a: expected_pickup(
+                    self.simulation.current_time,
+                    a,
+                    ind.pick_up_location,
+                    self.simulation.params,
+                    secondary=True
+                )
+                for a in range(self.simulation.network.number_of_nodes - 1)
+            }
+            ordered_choices = sorted(choices.keys(), key=lambda x: choices[x])
+            for choice in ordered_choices:
+                node = self.simulation.nodes[choice + 2]
+                for s in node.servers:
+                    if not s.busy:
+                        ind.rrv = node.id_number - 2
+                        return node
+            return self.simulation.nodes[-1]
+
+    def write_individual_record(self, individual):
+        """
+        Write a data record for an individual:
+        - id_number
+        - rrv_location
+        - rrv_id
+        - rrv_service_time
+        - rrv_pick_up_time
+        - rrv_delay_at_site
+        - rrv_return_to_loc_time
+        """
+        if math.isinf(self.c):
+            server_id = False
+        else:
+            server_id = individual.server.id_number
+
+        record = ResponseDataRecord(
+            individual.id_number,
+            self.id_number - 2,
+            f"L{individual.rrv} V{server_id}",
+            individual.service_end_date - individual.service_start_date,
+            individual.pick_up_time,
+            individual.delay_at_site,
+            individual.return_to_loc_time,
+        )
+        individual.data_records.append(record)
+
+
+class ResponseArrivalNode(ciw.ArrivalNode):
+    def have_event(self):
+        """
+        Finds a batch size. Creates that many Individuals and send
+        them to the relevent node. Then updates the event_dates_dict.
+        """
+        batch = self.batch_size(self.next_node, self.next_class)
+        for _ in range(batch):
+            self.number_of_individuals += 1
+            self.number_of_individuals_per_class[self.next_class] += 1
+            priority_class = self.simulation.network.priority_class_mapping[
+                self.next_class]
+            row = self.simulation.initial_recs.iloc[self.number_accepted_individuals]
+            next_individual = self.simulation.IndividualType(
+                row['id_number'],
+                row,
+                self.next_class,
+                priority_class,
+                simulation=self.simulation)
+            if self.simulation.network.process_based:
+                next_individual.route = self.simulation.network.customer_classes[
+                next_individual.customer_class].routing[self.next_node - 1](next_individual)
+            next_node = self.simulation.transitive_nodes[self.next_node - 1]
+            self.release_individual(next_node, next_individual)
+
+        self.event_dates_dict[self.next_node][
+            self.next_class] = self.increment_time(
+            self.event_dates_dict[self.next_node][
+            self.next_class], self.inter_arrival(
+            self.next_node, self.next_class))
+        self.find_next_event_date()
+
+
+def get_service_response(initial_call_time, ind, params):
+    """
+    The expected service time for rrv a to service patient p in time period t.
+    Consists of:
+      - getting to the patient
+      - delay at site
+      - getting back to rrv location from the hospital
+    """
+    a, p, k = ind.rrv, ind.pick_up_location, ind.speciality
+    expected_pick_up_transit_time = journey_time(initial_call_time, params["amb_to_patient"][a][p], params, secondary=True)
+    ind.expected_pick_up_time = expected_pick_up_transit_time
+    if ind.expected_pick_up_time > ind.expected_ambulance_pick_up_time:
+        ind.complete_time = 0
+    else:
+        pick_up_transit_time = ciw.random.expovariate(1 / expected_pick_up_transit_time)
+        ind.pick_up_time = pick_up_transit_time
+
+        ind.delay_at_site = max(ind.ambulance_pick_up_time + ind.ambulance_delay_at_site - ind.pick_up_time, 0)
+        
+        expected_return_to_loc_time = journey_time(initial_call_time, params["patient_to_amb"][p][a], params, secondary=True)
+        return_to_loc_time = ciw.random.expovariate(1 / expected_return_to_loc_time)
+        ind.return_to_loc_time = return_to_loc_time
+        
+        ind.complete_time = ind.pick_up_time + ind.delay_at_site + ind.return_to_loc_time
+
+
+def make_response_service_dist(params):
+    class ResponseTrip(ciw.dists.Distribution):
+        def sample(self, t, ind):
+            get_service_response(t, ind, params)
+            return ind.complete_time
+
+    return ResponseTrip()
+
+
+def create_response_network(params, initial_recs):
+    arrivals = [list(initial_recs['call_date'])[0]] + list(initial_recs['call_date'].diff())[1:] + [float('inf')]
+    N = ciw.create_network(
+        arrival_distributions=[ciw.dists.Sequential(arrivals)] + [ciw.dists.NoArrivals() for _ in range(params['n_ambulances'])],
+        service_distributions=[ciw.dists.Deterministic(0)] + [make_response_service_dist(params) for _ in range(params['n_ambulances'])],
+        number_of_servers=[float("Inf")] + params["ambulance_allocation"],
+        routing=[[0 for _ in range(params["n_ambulances"] + 1)] for _ in range(params["n_ambulances"] + 1)]
     )
     return N
